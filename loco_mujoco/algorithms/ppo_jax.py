f52469a0488d659404d0fc5eb9574958afe7ca8e
Friezer <frisoholleman@gmail.com>
2026-05-08 15:10:36 +0200

    Files and training edit

--- loco_mujoco/algorithms/ppo_jax.py
+++ loco_mujoco/algorithms/ppo_jax.py
@@ -125,7 +125,7 @@ class PPOJax(JaxRLAlgorithmBase):
                 optax.clip_by_global_norm(config.experiment.max_grad_norm),
                 optax.adamw(weight_decay=config.experiment.weight_decay, eps=1e-5,
                             learning_rate=lambda count: cls._linear_lr_schedule(count, config.experiment.num_minibatches,
-                                                                                config.experiment.update_epochs, config.lr,
+                                                                                config.experiment.update_epochs, config.experiment.lr,
                                                                                 config.experiment.num_updates))
             )
         else:
